212dc2099fb5ccf4b7178b06bc43123aa66cf5f1
Damir Harambasic <dharambasic@overstock.com>
2019-05-02 22:51:50 -0600

    trying stuffs

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import { Link } from 'gatsby';
 
-import Header from '../components/header/Header.js';
+import Header from '../components/Header/Header';
 import './global.scss';
 import favicon from '../images/favicon.ico';
 
