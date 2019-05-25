4a487875fb3f626e5b88ffeb07417f54fc56f59a
Damir Harambasic <dharambasic@overstock.com>
2019-05-25 10:11:00 -0600

    added in height for spotify and changed around some logic

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 import './footer.scss';
 
 class Footer extends Component {
   state = {
-    loadingSpotifyData: true,
     spotifyData: null,
   }
 
@@ -20,34 +19,29 @@ class Footer extends Component {
             loadingSpotifyData: false,
           });
         });
-    } else {
-      this.setState({
-        loadingSpotifyData: false,
-      });
     }
   }
 
   render() {
     return (
       <footer className="footer">
-        {!this.state.loadingSpotifyData
-          ? (
-            this.state.spotifyData && (
-              <div className="now-playing">
-                {this.state.spotifyData.image
-                  && (
-                    <span className="album-cover">
-                      <img alt="album-cover" src={this.state.spotifyData.image.find(image => image.size === 'small')['#text']} />
-                    </span>
-                  )
-                }
-                <span>#NowListening: </span>
-                {this.state.spotifyData.artist
-                  && <span className="artist">{this.state.spotifyData.artist['#text']}</span>
-                } - <span className="song-name">{this.state.spotifyData.name}</span>
-              </div>
-            )
-          ) : <div className="now-playing">Loading Spotify Data...</div>}
+        <div className="now-playing">
+          {this.state.spotifyData && (
+            <Fragment>
+              {this.state.spotifyData.image
+              && (
+                <span className="album-cover">
+                  <img alt="album-cover" src={this.state.spotifyData.image.find(image => image.size === 'small')['#text']} />
+                </span>
+              )}
+              <span>#NowListening: </span>
+              {this.state.spotifyData.artist
+                && <span className="artist">{this.state.spotifyData.artist['#text']}</span>
+              }
+              {this.state.spotifyData && <span className="song-name"> - {this.state.spotifyData.name}</span>}
+            </Fragment>
+          )}
+        </div>
       </footer>
     );
   }
