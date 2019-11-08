3fb431c8a2849ba8b229b83bc351b08844afaec6
Damdeez <damir.hara@gmail.com>
2019-11-08 13:21:42 -0700

    made Footer a functional component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,48 +1,42 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 
 import './footer.scss';
 
-class Footer extends Component {
-  state = {
-    spotifyData: null,
-  }
+const Footer = () => {
+  const [spotifyData, setSpotifyData] = useState(null);
 
-  componentDidMount() {
+  useEffect(() => {
     if (window.fetch) {
       window.fetch('//ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=damdeez&api_key=450b8037e3670e92714d68ea8dd86f1e&limit=1&format=json', {
         method: 'GET',
       }).then((response) => response.json())
         .then((data) => {
-          const spotifyData = data.recenttracks && data.recenttracks.track[0];
-          this.setState({
-            spotifyData,
-          });
+          const spotifyDataRecent = data.recenttracks && data.recenttracks.track[0];
+          setSpotifyData(spotifyDataRecent);
         });
     }
-  }
+  }, []);
 
-  render() {
-    return (
-      <footer className="footer">
-        <div className="now-playing">
-          {this.state.spotifyData && (
-            <Fragment>
-              {this.state.spotifyData.image
-              && (
-                <span className="album-cover">
-                  <img alt="album-cover" src={this.state.spotifyData.image.find((image) => image.size === 'medium')['#text']} />
-                </span>
-              )}
-              <span className="now-listening">#NowListening: </span>
-              {this.state.spotifyData.artist
-                && <span className="artist">{this.state.spotifyData.artist['#text']}</span>}
-              {this.state.spotifyData.name && <span className="song-name"> - {this.state.spotifyData.name}</span>}
-            </Fragment>
-          )}
-        </div>
-      </footer>
-    );
-  }
-}
+  return (
+    <footer className="footer">
+      <div className="now-playing">
+        {spotifyData && (
+          <Fragment>
+            {spotifyData.image
+            && (
+              <span className="album-cover">
+                <img alt="album-cover" src={spotifyData.image.find((image) => image.size === 'medium')['#text']} />
+              </span>
+            )}
+            <span className="now-listening">#NowListening: </span>
+            {spotifyData.artist
+              && <span className="artist">{spotifyData.artist['#text']}</span>}
+            {spotifyData.name && <span className="song-name"> - {spotifyData.name}</span>}
+          </Fragment>
+        )}
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
