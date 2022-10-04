8503fe2555c459542ec8494a7c3ab6f53c47b93f
damdeez <damir.hara@gmail.com>
2022-10-04 11:26:29 -0600

    couple small tweaks

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 import HelmetMeta from '../components/Meta/HelmetMeta';
 import Main from '../components/Main/Main';
-import NowPlaying from '../components/NowPlaying/NowPlaying';
 import './global.scss';
 
 const App = () => (
@@ -10,7 +9,6 @@ const App = () => (
     <HelmetMeta pageTitle="Web Development by Damir" />
     {/* TODO: refactor the Header since its not really a header */}
     <Main />
-    <NowPlaying />
   </main>
 );
 
