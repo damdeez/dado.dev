0e07e600d13c6f124101a338197443a3948980d1
damdeez <damir.hara@gmail.com>
2022-10-04 11:28:49 -0600

    get rid of header

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -6,15 +6,15 @@ import damirJpg from '../../files/damir.jpg';
 import NowPlaying from '../NowPlaying/NowPlaying';
 
 const Main = () => (
-  <header className={styles.header}>
+  <main className={styles.main}>
     <img className={styles.damirImg} src={damirJpg} alt="Damir" />
-    <div className={styles.headerText}>
+    <div className={styles.mainText}>
       <h1>{'>'} Hi, my name is Damir.</h1>
       <h3>I am a Software Engineer focused on building aesthetically pleasing Web-based products. You can check out my resume <Link to="/resume">here</Link>. You can also find me on these social mediums: <a href="https://github.com/damdeez">Github</a>, <a href="https://twitter.com/damdeez">Twitter</a>, <a href="https://www.linkedin.com/in/damirharambasic">LinkedIn</a>, or feel free to send me a good ol&apos; <a href="mailto:yes@damir.fun">email</a>. ☕️
       </h3>
       <NowPlaying />
     </div>
-  </header>
+  </main>
 );
 
 export default Main;
